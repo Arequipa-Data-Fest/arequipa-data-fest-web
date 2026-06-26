2eb15db0f520df7f9d069ec3a50fe14f85d7e149
SaulSivincha <ssivincham@unsa.edu.pe>
2026-06-26 02:44:46 -0500

    feat:a lot of things

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import cloudflare from '@astrojs/cloudflare';
 // https://astro.build/config
 export default defineConfig({
   adapter: cloudflare({
-    platformProxy: { enabled: true },
     imageService: 'passthrough'
   }),
 
